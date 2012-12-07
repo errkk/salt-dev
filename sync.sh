249f68248e27387ed943e05a6727748b4051dd6f
Eric George <eric@pokelondon.com>
2012-12-07 16:56:32 +0000

    Git config

--- sync.sh
+++ sync.sh
@@ -1,3 +1,6 @@
 #! /bin/sh
-
+echo 'Copying .vimrc'
 cp ~/checkouts/dotfiles/.vimrc ~/.salt-dev/states/dev_vim/files/.vimrc
+
+echo 'Copying .gitconfig'
+cp ~/checkouts/dotfiles/.gitconfig ~/.salt-dev/states/dev_git/files/.gitconfig
